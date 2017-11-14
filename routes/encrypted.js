cc710d0c90d79f994cb40322444234e623176c67
Vicroni <drasa_tec@hotmail.com>
2017-11-13 20:42:43 -0600

    Agregado parte del kerberos

--- routes/encrypted.js
+++ routes/encrypted.js
@@ -1,9 +1,12 @@
-var encriptador  = require("./../security/des").des;
+var RSA  = require("./../security/rsa");
+var DES  = require("./../security/des").des;
 exports.des = function (request, response) {
     response.writeHead(200, { 'Content-Type': 'application/json' });
     let clave  = "0001001100110100010101110111100110011011101111001101111111110001";
     let cadena = "0000000100100011010001010110011110001001101010111100110111101111";
 
-    console.log(cadena == encriptador.desicifrar(encriptador.cifrar(cadena, clave), clave));
+    console.log(cadena == DES.desicifrar(DES.cifrar(cadena, clave), clave));
     response.end("Todo bien");
+
+    console.log('RSA: ' + JSON.stringify(RSA.generatorKeys(3, 7)));
 };
